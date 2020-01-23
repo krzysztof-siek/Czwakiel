c1720263c4826b9ccf27dd9d6efcf625b821142c
krzysztof-siek <krzysztof.siek01@gmail.com>
2020-01-23 15:30:29 +0100

    fix some responsive problem

--- js/app.js
+++ js/app.js
@@ -1,14 +1,31 @@
 window.addEventListener('load', (event) => {
 
     const navButton = document.querySelector(".nav-button")
+    const navLinks = document.querySelector('.nav-links');
 
     toggleNavLinks = () => {
-        const navLinks = document.querySelector('.nav-links');
         navLinks.classList.toggle("open")
+
 
     }
 
     navButton.addEventListener('click', toggleNavLinks);
+
+    fixButton = () => {
+        let scrollY = window.scrollY;
+        if (scrollY > 100) {
+            navButton.classList.add("fixed")
+            navLinks.classList.add('top')
+        } else {
+            navButton.classList.remove("fixed")
+            navLinks.classList.remove('top')
+        }
+    }
+
+    window.addEventListener('scroll', fixButton)
+
+
+
 
 
 })
